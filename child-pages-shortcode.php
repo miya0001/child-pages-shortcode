51725a6667c52369db0b1736f3aa2b566fd068bc
Takayuki Miyauchi <miya@theta.ne.jp>
2011-12-14 10:19:57 +0900

    update

--- child-pages-shortcode.php
+++ child-pages-shortcode.php
@@ -4,7 +4,7 @@ Plugin Name: Child Pages Shortcode
 Author: Takayuki Miyauchi
 Plugin URI: http://wpist.me/wp/child-pages-shortcode/
 Description: You can use shortcode for display child pages from the page.
-Version: 0.5.0
+Version: 0.6.0
 Author URI: http://wpist.me/
 Domain Path: /languages
 Text Domain: child-pages-shortcode
